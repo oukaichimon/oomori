06efebf0aee1af8ad17b2aeb876e3079028d41e8
oukaichimon <cxn01355@gmail.com>
2020-08-15 23:37:37 +0900

    owari

---
+++ Assets/_MyAssets/CameraChaser.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChaser : MonoBehaviour
+{
+
+    Transform myCamera;
+    Transform myTrans;
+
+    [Header("字幕とカメラからの距離")]
+    [SerializeField]
+    float distanse = 1;
+
+    [Header("字幕の高さ")]
+    [SerializeField]
+    float hight = -1;
+
+
+
+    [Header("字幕の追従速度 0.01fが標準速度")]
+    [SerializeField]
+    float lerp = 0.01f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTrans = this.transform;
+        myCamera = GameObject.FindGameObjectWithTag("MainCamera").transform;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //字幕が存在する理想地点
+        Vector3 v = myCamera.rotation * Vector3.forward * distanse + myCamera.rotation * Vector3.up * hight;
+        myTrans.position = Vector3.Lerp(myTrans.position, v, lerp);
+        myTrans.LookAt(myTrans.position * 2 -  myCamera.position);
+
+
+    }
+}
---
+++ Assets/_MyAssets/TimeKeepr.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeKeepr : MonoBehaviour
+{
+
+    [Header("この字幕が出る時間")]
+    [SerializeField]
+    float startTime;
+
+    [Header("この字幕が消える時間")]
+    [SerializeField]
+    float endTime;
+
+    Vector3 defaultV;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultV = this.transform.localScale;
+        this.transform.localScale = Vector3.one * 0.01f;
+
+        StartCoroutine(Jimaku());
+
+    }
+
+    IEnumerator Jimaku()
+    {
+        yield return new WaitForSeconds(startTime);
+
+        this.transform.localScale = defaultV;
+        yield return new WaitForSeconds(endTime - startTime);
+        this.transform.localScale = Vector3.one * 0.01f;
+
+    }
+
+}
